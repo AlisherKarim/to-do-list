229f510db98c63221566ef5bb75b1b30eac84521
Alisher Karim <alisherktlwik@gmail.com>
2020-08-17 16:03:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import uuid from 'uuid'
+import ToDoInput from "./components/ToDoInput"
+import TooDoList from "./components/TodoList"
+import ToDoItem from "./components/ToDoItem"
+import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoList/>
+        <ToDoInput/>
+        <ToDoItem/>
+        <h1>hello from app</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
