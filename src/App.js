6ade4db541941567ea1519238f97379a9a51946e
Alisher Karim <alisherkarim@github.com>
2021-11-02 20:04:16 +0900

    change class to function, use react hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,92 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState, useEffect } from "react";
 import {v4 as uuidv4} from 'uuid'
 import ToDoInput from "./components/ToDoInput"
 //import ToDoItem from "./components/ToDoItem"
 import TodoList from './components/TodoList'
 //some comment
+
+export default function App() {
+
+  const [id, setID] = useState(uuidv4());
+  const [items, setItems] = useState( () => {
+    
+    const localItems = localStorage.getItem("localItems");
+    const jsonized = JSON.parse(localItems)
+    return jsonized || []
+    // [
+    // {id: uuidv4(), text: "wake up"},
+    // {id: uuidv4(), text: "do exercise"}
+    // ]
+    }
+  )
+  const [item, setItem] = useState("");
+  const [edit, setEdit] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(edit === true){
+
+      setItems(items.filter((CurrentItem) => {
+        if(CurrentItem.id === id){
+          CurrentItem.text = item
+        }
+        return true;
+      }
+      )
+      )
+      setItem("");
+      setID(uuidv4());
+      setEdit(false);
+    }
+    else{
+      const newItem = {id: id, text: item}
+
+      setItems([...items, newItem])
+      setItem("")
+      setID(uuidv4())
+    }
+  }
+  const handleClear = () => {
+    setItems([]);
+  }
+  const handleEdit = (idOfEditable) => {
+    console.log(`Editing ${idOfEditable}`);
+    const itemToBeEdited = items.find((CurrentItem) => {return CurrentItem.id === idOfEditable})
+    setItem(itemToBeEdited.text);
+    setID(idOfEditable)
+    setEdit(true)
+  }
+  const handleDelete = (idOfDeletable) => {
+    //console.log(id)
+    setItems(items.filter((CurrentItem) => {return CurrentItem.id !== idOfDeletable}));
+  }
+  const handleChange = (event) => {
+    setItem(event.target.value);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("localItems", JSON.stringify(items));  
+  }
+  )
+
+  return (
+    <div>
+      <div className = "container">
+        <div className = "row">
+          <div className = "col-10 mx-auto col-md-8 mt-5"> 
+            <h2 className = "text-capitalize text-center">Your to-do list by <a class="text-lowercase" href = "https://github.com/alisherkarim">@alisherkarim</a></h2>
+            <ToDoInput handleSubmit = {handleSubmit} value = {item} handleChange = {handleChange}/>
+            <TodoList items = {items} handleClear = {handleClear} handleDelete = {handleDelete} handleEdit = {handleEdit}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+/*
 export default class App extends Component {
   state = {
     items : [
@@ -15,20 +98,23 @@ export default class App extends Component {
     edit: false
   }
 
+  
+
   handleSubmit = (event) => {
     event.preventDefault();
     if(this.state.edit === true){
       this.setState({
-        items: this.state.items.filter((item) => {
+        items: this.state.items.filter((item) => 
+        {
           if(item.id === this.state.id){
             item.text = this.state.item
           }
           return true
         }),
-        item: "",
-        id: uuidv4(),
-        edit: false
-      })
+          item: "",
+          id: uuidv4(),
+          edit: false
+        })
     }
     else{
       const newItem = {id: this.state.id, text: this.state.item}
@@ -65,13 +151,15 @@ export default class App extends Component {
     })
   }
   render() {
+
+
     //console.log(this.state.items)
     return (
       
       <div className = "container">
         <div className = "row">
           <div className = "col-10 mx-auto col-md-8 mt-5"> 
-            <h2 className = "text-capitalize text-center">To-Do List Project</h2>
+            <h2 className = "text-capitalize text-center">Your to-do list by <a class="text-lowercase" href = "https://github.com/alisherkarim">@alisherkarim</a></h2>
             <ToDoInput handleSubmit = {this.handleSubmit} value = {this.state.item} handleChange = {this.handleChange}/>
             <TodoList items = {this.state.items} handleClear = {this.handleClear} handleDelete = {this.handleDelete} handleEdit = {this.handleEdit}/>
           </div>
@@ -82,3 +170,4 @@ export default class App extends Component {
   }
 }
 
+*/
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,7 +7,7 @@ export default class TodoList extends Component {
         //console.log(items);
         return (
             <section style = {{margin: "4rem 0 0 0"}}>
-                <h3>To-Do List</h3>
+                <h3>Your List:</h3>
                 <ul className = "list-group">
                     {items.map((item) => <li className = "list-group-item" key = {item.id}><Item handleDelete = {handleDelete} handleEdit = {handleEdit} itemName = {item.text} itemId = {item.id}/></li>)}
                 </ul>
